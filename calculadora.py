517e2b084b5b18324f42adf373ec5dca58a9f61c
David Jimenez <correo@davidjimenez.dev>
2025-06-24 21:09:54 +0200

    Remake calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,41 @@
-while True:
-    try:
-        entrada = input("Introduce la operación y pulsa Enter (q para salir): ")
-        if entrada.lower() in ("q",):
-            break
-        print(eval(entrada))
-    except Exception:
-        print("La entrada no es correcta, utiliza los operadores predeterminados de Python")
+def suma(*args:float) -> float:
+    return sum(args)
+
+def resta(*args:float) -> float:
+    resultado = args[0]
+    for valor in args[1:]:
+        resultado -= valor
+    return resultado
+
+def multiplicacion(*args:float) -> float:
+    resultado = 1
+    for valor in args:
+        resultado *= valor
+    return resultado
+
+def division(*args:float) -> float:
+    resultado = args[0]
+    for valor in args[1:]:
+        resultado /= valor
+    return resultado
+
+def calculadora(operacion, *args):
+    if operacion == "+":
+        return suma(*args)
+    elif operacion == "-":
+        return resta(*args)
+    elif operacion == "*":
+        return multiplicacion(*args)
+    elif operacion == "/":
+        return division(*args)
+    else:
+        print("Operación no reconocida")
+
+
+
+operacion = input("¿Qué operación deseas hacer? (+,-,*,/): ")
+valores = input("Introduce los valores con los que quieres operar separados por espacios: ")
+
+valores = [float(valor) for valor in valores.split()]
+
+print(f"El resultado es {calculadora(operacion, *valores)}")
