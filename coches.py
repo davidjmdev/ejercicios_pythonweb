09e2e665ae2f25e8a09592653a7a68b6c4c42278
David Jimenez <correo@davidjimenez.dev>
2025-06-25 20:10:21 +0200

    Add coches.py

--- coches.py
+++ coches.py
@@ -1,3 +1,27 @@
 class Coche:
-    def __init__(self):
-        pass
+    def __init__(self, marca:str, modelo:str, color:str):
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+        self.conductor = None
+
+class Conductor:
+    def __init__(self, nombre:str, edad:int, carnet:bool):
+        self.nombre = nombre
+        self.edad = edad
+        self.carnet = carnet
+
+    def conducir(self, coche:Coche):
+        if self.carnet == True:
+            coche.conductor = self
+        else:
+            print("No puedes conducir sin carnet")
+
+
+
+Ibiza = Coche("Seat", "Ibiza", "Amarillo")
+Pedro = Conductor("Pedro", 20, True)
+
+Pedro.conducir(Ibiza)
+
+print(Ibiza.conductor.nombre)
